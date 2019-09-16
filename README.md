3bac5a5de293881c706031041db6f03a8e7cfe52
Gareth Rushgrove <gareth@morethanseven.net>
2019-09-15 17:32:02 -0700

    Tools to generate a Scoop Bucket based on updated Snyk releases

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scoop Bucket for Snyk
+
+![](https://github.com/garethr/scoop-snyk/workflows/Update%20Snyk%20Scoop%20bucket%20with%20latest%20release/badge.svg)
+
+Installing the bucket will provide access to Snyk software via [Scoop](https://scoop.sh/):
+
+```console
+scoop bucket add snyk https://github.com/garethr/scoop-snyk
+```
+
+Installing Snyk can then be done as follows:
+
+```console
+scoop install snyk
+```
---
+++ render.rb
@@ -0,0 +1,43 @@
+#! /usr/bin/env ruby
+
+#
+# This script grabs the latest release details from the GitHub API for a project
+# and then generates a Homebrew Formula.
+#
+# It currently assumes you have separate binaries for Linux and macOS and generates
+# a forumula which works with both homebrew and linuxbrew.
+#
+# This is intended for cases where projects release quickly and you want a Homebrew Tap
+# to track those releases closely, and where you don't have automation (like
+# Goreleaser) which can generate Homebrew Formula automatically.
+#
+
+require "erb"
+require "json"
+require "open-uri"
+
+PROJECT = "snyk/snyk"
+BIN = "snyk"
+
+# Return the URL, SHA and version information for a GitHub release
+def get_latest_release(project, bin)
+  api_url = "https://api.github.com/repos/#{project}/releases/latest"
+  data = URI.parse(api_url).read
+  obj = JSON.parse(data)
+  version = obj["name"]
+  sha_url = "https://github.com/#{project}/releases/download/#{version}/#{bin}.sha256"
+  sha = URI.parse(sha_url).read
+  url = "https://github.com/#{project}/releases/download/#{version}/#{bin}"
+  sha256 = sha.split(" ").first
+
+  return url, sha256, version
+end
+
+
+
+@url, @sha256, @version = get_latest_release(PROJECT, "#{BIN}-win.exe")
+
+template = File.read("template.erb")
+
+renderer = ERB.new(template)
+puts renderer.result()
